456d70afe88b92b448c8936b300f2d356f3f2af8
Bennett Critchley <bennett.critchley@hotmail.com>
2021-09-27 11:31:21 -0600

    adding main files

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+// import slugify from "slugify";
+import "./App.css";
+import Header from "./Header/Header.js";
+import CustomizeLaptop from "./Customize_Laptop/Customize_Laptop.js";
+import Cart from "./Cart/Cart.js";
+
+// This object will allow us to
+// easily convert numbers into US dollar values
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+class App extends Component {
+  state = {
+    selected: {
+      Processor: {
+        name: 'Intel Core I9 Max',
+        cost: 700
+      },
+      'Operating System': {
+        name: 'Windows 10 - 32Bit',
+        cost: 100.00
+      },
+      'Video Card': {
+        name: 'Nvidia Ge-Force RTX 2080TI.',
+        cost: 1450.00
+      },
+      Display: {
+        name: 'MSI Optix 27" 120Hz 1080p Monitor',
+        cost: 300
+      }
+    }
+  };
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <CustomizeLaptop
+            features={this.props.features}
+            selected={this.state.selected}
+            updateFeature={this.updateFeature}
+          />
+          <Cart selected={this.state.selected} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
